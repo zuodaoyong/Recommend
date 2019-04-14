45b33545ffefa20a08036601015be542ce86e479
home <1810473882@qq.com>
2019-04-14 11:30:32 +0800

    推荐

---
+++ DataLoader/src/main/scala/com/dataloader/DataLoder.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+//创建case类
+case class Movie(val mid: Int, val name: String, val descri: String, val timelong: String, val issue: String,
+                 val shoot: String, val language: String, val genres: String, val actors: String, val directors: String)
+case class Rating(var uid:Int,var mid:Int,var score:Double,var timestamp:Int)
+case class Tag(var uid:Int,var mid:Int,var tag:String,var timestamp:Int)
+object DataLoder{
+  val MOVIE_DATA_PATH = "F:\\git\\Recommend\\DataLoader\\src\\main\\resources\\movies.csv"
+  val RATING_DATA_PATH = "F:\\git\\Recommend\\DataLoader\\src\\main\\resources\\ratings.csv"
+  val TAG_DATA_PATH = "F:\\git\\Recommend\\DataLoader\\src\\main\\resources\\tags.csv"
+
+  def main(args:Array[String]): Unit ={
+
+    val config = Map(
+      "spark.cores" -> "local[*]"
+    )
+    //创建sparkConf
+    val sparkConf=new SparkConf().setAppName(DataLoder.getClass.getSimpleName).setMaster(config.get("spark.cores").get)
+    val sparkSession=SparkSession.builder().config(sparkConf).getOrCreate()
+    import sparkSession.implicits. _
+    //加载数据
+    var movieRDD=sparkSession.sparkContext.textFile(MOVIE_DATA_PATH)
+    println(movieRDD.first())
+  }
+}
