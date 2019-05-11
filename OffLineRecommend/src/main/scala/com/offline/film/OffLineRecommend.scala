04d733b591db16d2ab322905b72bd32d3b841c9f
Home <1525714537@qq.com>
2019-05-11 18:21:01 +0800

    离线推荐

--- OffLineRecommend/src/main/scala/com/offline/film/OffLineRecommend.scala
+++ OffLineRecommend/src/main/scala/com/offline/film/OffLineRecommend.scala
@@ -1,4 +1,4 @@
-package com.offline
+package com.offline.film
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
@@ -13,7 +13,6 @@ object OffLineRecommend {
     val sparkConf=new SparkConf().setAppName(OffLineRecommend.getClass.getSimpleName).setMaster(config("spark.cores"))
     //创建一个sparkSession
     val sparkSession=SparkSession.builder().config(sparkConf).getOrCreate()
-    import  sparkSession.implicits._
 
   }
 }
